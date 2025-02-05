53ae52171b4b16d64f2ebec3c8adcf85e3f4ea0a
Dan Rogers <danrog729@gmail.com>
2025-02-05 22:34:20 +0000

    added window initialisation

--- main.cpp
+++ main.cpp
@@ -3,11 +3,81 @@
 #include "include/glad/glad.h"
 #include "include/GLFW/glfw3.h"
 
+#include "main.h"
+
 int main()
+{
+	// Set up GLFW error handling and initialise GLFW
+	glfwSetErrorCallback(sterling_glfw_error_callback);
+	if (sterling_initialise_glfw(4, 6, GLFW_OPENGL_CORE_PROFILE) != 0)
+	{
+		return 1;
+	}
+
+	// Create a GLFW window
+	GLFWwindow* window;
+	if (sterling_create_glfw_window(&window, 800, 600, "Sterling", NULL, NULL) != 0)
+	{
+		return 1;
+	}
+
+	// Initialise GLAD
+	if (sterling_initialise_glad() != 0)
+	{
+		return -1;
+	}
+
+	// Set up the OpenGL viewport
+	glViewport(0, 0, 800, 600);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Swap buffers
+		glfwSwapBuffers(window);
+		// Poll events
+		glfwPollEvents();
+	}
+}
+
+static void sterling_glfw_error_callback(int id, const char* description)
+{
+	std::cerr << "ERROR::GLFW::" << id << "::" << description << "\n";
+}
+
+static inline int sterling_initialise_glfw(int majorVersion, int minorVersion, int profile)
 {
 	if (!glfwInit())
 	{
-		std::cerr << "Failed to initialise GLFW";
+		std::cerr << "ERROR::GLFW_INITIALISATION_FAILED";
+		return 1;
 	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, majorVersion);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minorVersion);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, profile);
+	return 0;
+}
 
+static int sterling_create_glfw_window(GLFWwindow** target, int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share)
+{
+	GLFWwindow* window = glfwCreateWindow(width, height, title, monitor, share);
+	if (window == NULL)
+	{
+		std::cerr << "ERROR::GLFW::WINDOW_CREATION_FAILED" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	*target = window;
+	return 0;
+}
+
+static inline int sterling_initialise_glad()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "ERROR::GLAD_INITIALISATION_FAILED" << std::endl;
+		return -1;
+	}
+	return 0;
 }
---
+++ main.h
@@ -0,0 +1,43 @@
+#ifndef STERLING_MAIN_H
+#define STERLING_MAIN_H
+
+/// <summary>
+/// Entry point for the program
+/// </summary>
+/// <returns>0 if exitted successfully</returns>
+int main();
+
+/// <summary>
+/// Allows GLFW to print error messages to the console
+/// </summary>
+/// <param name="id">The ID of the error</param>
+/// <param name="description">The description of the error</param>
+static void sterling_glfw_error_callback(int id, const char* description);
+
+/// <summary>
+/// Initialises GLFW with a given version and profile
+/// </summary>
+/// <param name="majorVersion">The major version</param>
+/// <param name="minorVersion">The minor version</param>
+/// <param name="profile">The profile</param>
+static inline int sterling_initialise_glfw(int majorVersion, int minorVersion, int profile);
+
+/// <summary>
+/// Creates a GLFW window and makes it the current context
+/// </summary>
+/// <param name="target">The pointer to set to the address of the window</param>
+/// <param name="width">The width of the window</param>
+/// <param name="height">The height of the window</param>
+/// <param name="title">The title of the window</param>
+/// <param name="monitor">The monitor for the window to open on. Defaults to primary monitor if NULL</param>
+/// <param name="share">Another window to share a context with. Defaults to no other window if NULL</param>
+/// <returns>0 if window successfully created</returns>
+static int sterling_create_glfw_window(GLFWwindow** target, int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share);
+
+/// <summary>
+/// Initialises GLAD
+/// </summary>
+/// <returns>0 if successful</returns>
+static inline int sterling_initialise_glad();
+
+#endif
